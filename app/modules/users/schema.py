bf0684145b5389875ccf177b8fc8c9744358fa9b
Baraka mukelenga <baraka.m@code-care.pro>
2024-11-05 01:46:36 +0200

    Refactor user schema to include additional fields and enhance model structure

--- app/modules/users/schema.py
+++ app/modules/users/schema.py
@@ -1,7 +1,29 @@
 from pydantic import BaseModel
 from typing import Optional, List
 from uuid import UUID
+from datetime import datetime
 from app.modules.enrollments.schema import EnrollmentResponse
+
+
+class User(BaseModel):
+    id: UUID
+    first_name: str
+    last_name: str
+    surname: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    photo: Optional[str] = None
+    address: Optional[str] = None
+    sub: Optional[str] = None
+    provider: Optional[str] = None
+    account_confirmed_at: Optional[datetime] = None
+    profile_completed_at: Optional[datetime] = None
+    created_at: datetime
+    updated_at: datetime
+    enrollments: List[EnrollmentResponse]
+
+    class Config:
+        from_attributes = True
 
 
 class UserCreate(BaseModel):
@@ -13,7 +35,7 @@ class UserCreate(BaseModel):
     provider: Optional[str] = None
 
 
-class UserResponse(UserCreate):
+class UserResponse(User):
     id: UUID
     status: Optional[str] = "active"
 
